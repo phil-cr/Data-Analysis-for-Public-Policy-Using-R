210f82780b35d14d3c6346242a323fac53116b90
lena-harris <165847966+lena-harris@users.noreply.github.com>
2024-04-05 13:23:08 -0400

    ResearchProjectDraft.R

--- Final/ResearchProjectDraft.R
+++ Final/ResearchProjectDraft.R
@@ -1,85 +1,45 @@
 library(tidyverse)
 library(dplyr)
-library(eeptools)
+library(tidycensus)
 
-# reading csv file
-temp13 <- read.csv("Awards By Contractor Type (FY2013).csv", header = TRUE,
+#code for reading in the final singular and cleaned dataframes from each year
+temp2013 <- read.csv("FederalContracts2013.csv", header = TRUE,
                          stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
-temp13$year <- 2013
 
-# reading csv file
-temp14 <- read.csv("Awards By Contractor Type (FY2014).csv", header = TRUE,
+temp2014 <- read.csv("FederalContracts2014.csv", header = TRUE,
                          stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
 
-temp14$year <- 2014
-
-# reading csv file
-temp15 <- read.csv("Awards By Contractor Type (FY2015).csv", header = TRUE,
+temp2015 <- read.csv("FederalContracts2014.csv", header = TRUE,
                          stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
 
-temp15$year <- 2015
-
-# reading csv file
-temp16 <- read.csv("Awards By Contractor Type (FY2016).csv", header = TRUE,
+#code to read in the csv gov contracting files
+temp13_1 <- read.csv("FY2013_All_Contracts_Full_2013_1.csv", header = TRUE,
                          stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
 
-temp16$year <- 2016
+temp13_2 <- read.csv("FY2013_All_Contracts_Full_2013_2.csv", header = TRUE,
+                         stringsAsFactors = FALSE, colClass ="factor")
 
-# reading csv file
-temp17 <- read.csv("Awards By Contractor Type (FY2017).csv", header = TRUE,
-                   stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
+temp15 <- read.csv("FY2013_All_Contracts_Full_2013_3.csv", header = TRUE,
+                         stringsAsFactors = FALSE, colClass ="factor")
 
-temp17$year <- 2017
+#code to remove the necessary columns (repeat for all three dataframes in a given year)
+temp2013_1 <- temp13_1 %>% select(alaskan_native_corporation_owned_firm, american_indian_owned_business, 
+indian_tribe_federally_recognized, native_hawaiian_organization_owned_firm, tribally_owned_firm, 
+minority_owned_business, asian_pacific_american_owned_business, action_date_fiscal_year, 
+subcontinent_asian_asian_indian_american_owned_business, black_american_owned_business,
+hispanic_american_owned_business, native_american_owned_business,
+other_minority_owned_business, contracting_officers_determination_of_business_size_code,
+historically_underutilized_business_zone_hubzone_firm, recipient_city_name, recipient_name, 
+awarding_agency_name, federal_action_obligation, total_dollars_obligated, recipient_country_code, 
+recipient_county_name, recipient_state_code, recipient_state_name, number_of_actions, recipient_zip_4_code, 
+recipient_address_line_1, recipient_address_line_2)
 
-# reading csv file
-temp18 <- read.csv("Awards By Contractor Type (FY2018).csv", header = TRUE,
-                   stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
+#code to combine all three dataframes from a single year
+temp14<- temp2014_1 %>% rbind(temp2014_2) %>% 
+  rbind(temp2014_3)
 
-temp18$year <- 2018
-
-# reading csv file
-temp19 <- read.csv("Awards By Contractor Type (FY2019).csv", header = TRUE,
-                   stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
-
-temp19$year <- 2019
-
-# reading csv file
-temp20 <- read.csv("Awards By Contractor Type (FY2020).csv", header = TRUE,
-                   stringsAsFactors = FALSE, colClass ="factor")
-
-# add column for year 2020
-
-temp20$year <- 2020
-
-# reading csv file
-temp21 <- read.csv("Awards By Contractor Type (FY2021).csv", header = TRUE,
-                   stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
-
-temp21$year <- 2021
-
-# reading csv file
-temp22 <- read.csv("Awards By Contractor Type (FY2022).csv", header = TRUE,
-                   stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
-
-temp22$year <- 2022
-
-# reading csv file
-temp23 <- read.csv("Awards By Contractor Type (FY2023).csv", header = TRUE,
-                   stringsAsFactors = FALSE, colClass ="factor")
-# add column for year
-
-temp23$year <- 2023
-
-summary(contracttemp)
+#code to download the newly binded dataframe from one single year into a .csv file on your computer
+write.csv(temp13, file = "FederalContracts2014.csv")
 
 # getting rid of the dollar signs
 contract <- contracttemp %>% 
